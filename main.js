65b32ad2a35c1055f69efc67e43c6b1998e28859
nellycaitano <caitanonelly@gmail.com>
2024-09-15 15:58:22 +0200

    Animation add

--- main.js
+++ main.js
@@ -1,24 +1,90 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", function () {
+  
+  document.getElementById('shuffle-btn').addEventListener('click', function() {
+    let fstSection = document.getElementById('fst-section');
+    let sndSection = document.getElementById('snd-section');
+    
+    let fstDivs = fstSection.children;
+    if (fstDivs.length >= 2) {
+
+      fstDivs[0].classList.add('move-left');
+      fstDivs[1].classList.add('move-right');
+
+      // Once the animation is over, we make the exchange
+      setTimeout(function() {
+        // Delete motion classes
+        fstDivs[0].classList.remove('move-left');
+        fstDivs[1].classList.remove('move-right');
+
+        // Invert divs
+        fstSection.insertBefore(fstDivs[1], fstDivs[0]);
+
+        // Add a class for a slow, fluid return
+        fstDivs[0].classList.add('reset-transform');
+        fstDivs[1].classList.add('reset-transform');
+
+        // Remove class after return to allow animation to be replayed later
+        setTimeout(function() {
+          fstDivs[0].classList.remove('reset-transform');
+          fstDivs[1].classList.remove('reset-transform');
+        }, 1000); // Return time (1 second)
+      }, 500);  // Corresponds to duration of initial transition (0.5s)
+    }
+    
+    let sndDivs = sndSection.children;
+    if (sndDivs.length >= 2) {
+      sndDivs[0].classList.add('move-up');
+      sndDivs[1].classList.add('move-down');
+
+      setTimeout(function() {
+        sndDivs[0].classList.remove('move-up');
+        sndDivs[1].classList.remove('move-down');
+
+        sndSection.insertBefore(sndDivs[1], sndDivs[0]);
+
+        sndDivs[0].classList.add('reset-transform');
+        sndDivs[1].classList.add('reset-transform');
+
+        setTimeout(function() {
+          sndDivs[0].classList.remove('reset-transform');
+          sndDivs[1].classList.remove('reset-transform');
+        }, 1000);  
+      }, 500);  
+    }
+
+    let sct1 = document.getElementById('sct-1');
+    let sct2 = document.getElementById('sct-2');
+    
+    if (sct1 && sct2) {
+      sct1.classList.add('move-left');
+      sct2.classList.add('move-right');
+
+      setTimeout(function() {
+        sct1.classList.remove('move-left');
+        sct2.classList.remove('move-right');
+
+        // Create a temporary node to facilitate inversion
+        let tempNode = document.createElement('div');
+        sct1.parentNode.insertBefore(tempNode, sct1);
+
+        // Swapping places
+        sct2.parentNode.insertBefore(sct1, sct2);
+        tempNode.parentNode.insertBefore(sct2, tempNode);
+
+        // Delete the temporary node
+        tempNode.remove();
+
+        sct1.classList.add('reset-transform');
+        sct2.classList.add('reset-transform');
+
+        setTimeout(function() {
+          sct1.classList.remove('reset-transform');
+          sct2.classList.remove('reset-transform');
+        }, 1000); 
+      }, 500);  
+    }
+  });
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,25 @@ export default {
         'write':'Montserrat',
       },
 
+      scale: {
+        '120': '1.2',
+        '130': '1.3',
+      },
+
       boxShadow:{
         'effect':'0px 4px 6px rgba(0, 0, 0, 0.15)',
-      }
+      },
+
+      perspective: {
+        '1000': '1000px',  
+      },
+
+      rotate: {
+        'x-12': '12deg',
+        'x-6': '6deg',
+        'y-12': '12deg',
+        'y-6': '6deg',
+      },
     },
   },
   plugins: [],
